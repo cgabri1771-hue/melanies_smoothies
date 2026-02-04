2bb1a888181a26d3b440922c4ba0f27c91099580
cgabri1771-hue <cgabri1771@gmail.com>
2026-02-04 14:09:17 -0300

    Update streamlit_app.py
    fix typo in definition sf_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
 #sf.df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
-sf.df = st.dataframe(data=smoothiefroot_response.json(), width = 'stretch')
+sf_df = st.dataframe(data=smoothiefroot_response.json(), width = 'stretch')
